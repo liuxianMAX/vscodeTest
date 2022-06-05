6d0a04d4683baa3dcc5ba134b829b544148b0330
liuxian <liuxian_123789@163.com>
2022-06-05 19:28:22 +0800

    master

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <my-addition></my-addition>
+    <p>-------------------------------------</p>
+    <my-subtraction></my-subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Addition.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  components: {
+    'my-addition': Addition,
+    'my-subtraction': Subtraction,
+  },
+}
+</script>
